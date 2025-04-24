dfc24e42b582ea7b7a7898a2861909b8aa4df153
syed <syedhasnain9163@gmail.com>
2025-04-25 00:20:03 +0530

    Making production ready changes

---
+++ server.js
@@ -0,0 +1,57 @@
+const connectDB = require('./config/connectDB');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const credentials = require('./middlewares/credentials');
+const express = require('express');
+const mongoose = require('mongoose');
+const verifyJWT = require('./middlewares/verifyJWT');
+const path = require('path')
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 5500;
+
+connectDB();
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+//middleware for cookies
+app.use(cookieParser());
+
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+// routes
+app.use('/api/register', require('./routes/register'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/refresh', require('./routes/refresh'));
+app.use('/api/logout', require('./routes/logout'));
+
+app.use(verifyJWT);
+
+app.use('/api/manage-room', require('./routes/manageRoom'));
+app.use('/api/university-exam', require('./routes/universityExam'));
+app.use('/api/seat-allocation', require('./routes/seatAllocation'));
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => { console.log(`Server running on port ${PORT}...`) });
+});
+
+module.exports = app
